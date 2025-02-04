eb5085850317abd8e5a7ff54edc01aadf67a02b6
Mille <mille.mbvaaland@gmail.com>
2025-02-04 18:31:18 +0100

    Initial commit: Setup Next.js with TypeScript, Tailwind, and Sass

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-3xl font-bold text-red-500">
+        This text should be red if Tailwind is working
+      </h1>
+    </main>
+  );
+}
